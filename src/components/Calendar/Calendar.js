e51858bbfbdaf84628b32495d85854c68ed20405
Teddy <t.boirin@icloud.com>
2020-02-24 14:20:36 +0100

    fix-calendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Calendar.scss";
-
+import next_mouth from '../../asset/mois--change.svg'
+import previous_mouth from '../../asset/mois-previous.svg'
 class Calendar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       openFav: false,
-      tabDate : []
+      tabDate : [],
+      mouth: false,
+      date: "Juillet"
     };
     this.onSearching = this.onSearching.bind(this);
   }
@@ -18,15 +21,27 @@ class Calendar extends React.Component {
     });
   }
 
-  generateDay = (number, start) => {
-    for (let i = start; i < number + start; i++) {
-      this.state.tabDate.push(  <li key={i+"-days"}>{i}</li>)
+  nextMouth = () => {
+    if(this.state.mouth == false) {
+      document.getElementById('first-day').classList.toggle('none');
+      document.getElementById('second-day').classList.toggle('none');
+      this.setState({mouth: true})
+      this.setState({date: "Août"})
     }
-    for (let i = 1; i < 12; i++) {
-      this.state.tabDate.push(  <li key={i+"-days"}>{i}</li>)
+
+};
+
+  previousMouth = () => {
+    if(this.state.mouth == true) {
+      document.getElementById('first-day').classList.toggle('none');
+      document.getElementById('second-day').classList.toggle('none');
+      this.setState({mouth: false})
+      this.setState({date: "Juillet"})
+
     }
-    return this.state.tabDate;
+
   };
+
 
   render() {
     return (
@@ -36,8 +51,18 @@ class Calendar extends React.Component {
           calendrier <i className="icon-calendar"></i>
         </button>
         {this.state.openFav === true ? (
-          <div className="dateWrapper">
+          <div className="dateWrapper"> 
+            <div className="mouth">
+              <div className="mouth--previous">
+                <img src={previous_mouth} alt="change_mouth" onClick={this.previousMouth}/>
 
+              </div>
+               <span>{this.state.date} 2024</span>
+              <div className="mouth--next">
+                <img src={next_mouth} alt="change_mouth" onClick={this.nextMouth}/>
+              </div>
+
+            </div>
             <div className="day--container">
               <ul className="weekdays">
                 <li>Ven</li>
@@ -48,8 +73,26 @@ class Calendar extends React.Component {
                 <li>Mer</li>
                 <li>Jeu</li>
               </ul>
-              <ul className="days">
-                {this.generateDay(6, 26)}
+              <ul className="days" id={"first-day"}>
+                <li><span>26</span></li>
+                <li><span>27</span></li>
+                <li><span>28</span></li>
+                <li><span>29</span></li>
+                <li><span>30</span></li>
+                <li><span>31</span></li>
+              </ul>
+              <ul className="days none" id={"second-day"}>
+                <li><span className={"special"}>1</span></li>
+                <li><span className={"special"}>2</span></li>
+                <li><span className={"special"}>3</span></li>
+                <li><span className={"special"}>4</span></li>
+                <li><span className={"special"}>5</span></li>
+                <li><span className={"special"}>6</span></li>
+                <li><span className={"special"}>7</span></li>
+                <li><span className={"special"}>8</span></li>
+                <li><span className={"special"}>9</span></li>
+                <li><span className={"special"}>10</span></li>
+                <li><span className={"special"}>11</span></li>
               </ul>
             </div>
           </div>
