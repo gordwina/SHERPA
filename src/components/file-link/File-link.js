2ad2eb2018cfd4c00be70e015cd014becfffefee
alice-tkt <alice@wertkt.com>
2020-02-25 23:49:50 +0100

    clean up - file-link

--- src/components/file-link/File-link.js
+++ src/components/file-link/File-link.js
@@ -3,23 +3,26 @@ import { Redirect } from "react-router-dom";
 import "./File-link.scss";
 
 class FileLink extends React.Component {
+ 
   constructor(props) {
     super(props);
     this.state = {
-      Redirect: false
-    };
+     
+      
+    }; 
+   
   }
 
   render() {
     return (
       <div>
-        <a href="/informations">
+        <a href="/informations"> 
           <div className="FileLink">
             <div className="FileLinkImage"></div>
             <div className="FileLinkInformation">
               <p className="FileLinkLocation">
                 {" "}
-                <strong>{this.props.location}</strong>
+                <strong>{this.props.nom}</strong>
               </p>
               <p className="FileLinkCapacity">
                 <i className="icon-people"></i> {this.props.capacity}{" "} capacité
